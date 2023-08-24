6ab0d49effd7ff5bcd95df26b1fc6618afcb944b
Mapat <khadonov.marat@yandex.ru>
2023-08-24 14:05:26 +0300

    first commit

---
+++ polyhedron-main/run_shadow.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S python3 -B
+
+from time import time
+from common.tk_drawer import TkDrawer
+from shadow.polyedr import Polyedr
+
+
+tk = TkDrawer()
+try:
+    for name in ["test2", "test1", "ccc", "cube", "box", "king", "cow"]:
+        print("=============================================================")
+        print(f"Начало работы с полиэдром '{name}'")
+        start_time = time()
+        pol = Polyedr(f"data/{name}.geom")
+        print(f"Площадь граней = {pol.ret_sqr()}")
+        pol.draw(tk)
+        delta_time = time() - start_time
+        print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
+        input("Hit 'Return' to continue -> ")
+except (EOFError, KeyboardInterrupt):
+    print("\nStop")
+    tk.close()
